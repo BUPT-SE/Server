b115e1befbbb41b0c1acc44a2b5294bbebb57218
ChangeLynn <bian.love@qq.com>
2017-04-08 14:44:28 +0800

    first commit

---
+++ attribute.cpp
@@ -0,0 +1,66 @@
+#include "attribute.h"
+
+QString Attribute::getRoomNum() const
+{
+    return _roomNum;
+}
+
+void Attribute::setRoomNum(const QString &roomNum)
+{
+    _roomNum = roomNum;
+}
+
+int Attribute::getWindSpeed() const
+{
+    return _windSpeed;
+}
+
+void Attribute::setWindSpeed(const int &windSpeed)
+{
+    _windSpeed = windSpeed;
+}
+
+float Attribute::getRoomTmp() const
+{
+    return _roomTmp;
+}
+
+int Attribute::getMode() const
+{
+    return _mode;
+}
+
+float Attribute::getTargetTmp() const
+{
+    return _targetTmp;
+}
+
+void Attribute::setTargetTmp(float targetTmp)
+{
+    _targetTmp = targetTmp;
+}
+
+bool Attribute::getPower() const
+{
+    return _power;
+}
+
+void Attribute::setPower(bool power)
+{
+    _power = power;
+}
+
+bool Attribute::getIsServed() const
+{
+    return _isServed;
+}
+
+float Attribute::getKWh() const
+{
+    return _kWh;
+}
+
+float Attribute::getFee() const
+{
+    return _fee;
+}
---
+++ server.h
@@ -0,0 +1,49 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <QWidget>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QList>
+#include "clientblock.h"
+#include <QDateTime>
+#include <Qtimer>
+namespace Ui {
+class Server;
+}
+
+class Server : public QWidget
+{
+    Q_OBJECT
+public slots:
+    void newClient();//有从机建立连接请求时，建立TCP连接
+    void updateUI();//更新面板上各从机的状态
+public:
+    explicit Server(QWidget *parent = 0);
+    ~Server();
+    void schedule();//1.遍历queue把服务完成的从机放到队尾；2.调用qSort对服务未完成的从机排序（选前三），依据优先级
+    QTime time = QTime::currentTime();
+    int hour=time.hour();
+    int min=time.minute();
+
+    QDateTime dateTime=QDateTime::currentDateTime();
+    int year=dateTime.date().year();
+    int month=dateTime.date().month();
+    int day=dateTime.date().day();
+
+private slots:
+    void on_check1_clicked();
+    void on_check2_clicked();
+    void on_check3_clicked();
+    void on_check4_clicked();
+    void on_pushButton_clicked();
+//public slots:
+   // QDateTime dateTime=QDateTime::currentDateTime();
+    //int year=dateTim
+private:
+    Ui::Server *ui;
+    QTcpServer _server;
+    QList<ClientBlock* > _queue;//存四个从控机，最多前三个是正在被服务，
+};
+
+#endif // SERVER_H
